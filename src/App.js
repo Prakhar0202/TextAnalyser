721c5e0b6fd9968dce66eded8e87d35bbbff72c7
Prakhar0202 <prakhar02ver@gmail.com>
2024-07-25 12:51:17 +0530

    added Router

--- src/App.js
+++ src/App.js
@@ -1,53 +1,60 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import React, { useState} from 'react';
-import Alert from './components/Alert';
-
-
-
-// import About from "./components/About";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./components/About";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const [mode,setMode] = useState('light');
-  const [alert,setAlert] = useState(null);
-
-  const showAlert = (message,type)=> {
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
-  }, 1800);
-}
+    }, 1800);
+  };
 
-
-const  toggleMode = () => {
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#052c65';
-      showAlert("Dark mode has been enabled","success");
-      document.title = 'Text Analyser - Dark Mode';
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#052c65";
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "Text Analyser - Dark Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#ddd6e7";
+      showAlert("Light mode has been enabled", "success");
+      document.title = "Text Analyser - Light Mode";
     }
-    else{
-      setMode('light')
-      document.body.style.backgroundColor = '#ddd6e7';
-      showAlert("Light mode has been enabled","success");
-      document.title = 'Text Analyser - Light Mode';
-
-    }
-  }
+  };
 
   return (
     <>
-      <Navbar title="Text Analyser" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm heading="Enter the text to Analyse" showAlert={showAlert} mode={mode}/>
-        {/* <About/> */}
-      </div>
+      <Router>
+        <Navbar title="Text Analyser" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route
+             exact path="/"
+              element={
+                <TextForm
+                  heading="Enter the text to Analyse"
+                  showAlert={showAlert}
+                  mode={mode}
+                />
+              }
+            />
+            <Route exact path="/About" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 
 export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg   "  style={{backgroundColor: props.mode==='dark'?'black':'rgb(19 160 189)'}}  >
     <div className="container-fluid">
-      <a className="navbar-brand " style = {{color: props.mode==='dark'?'white':'black'}} href="/">{props.title}</a>
+      <Link className="navbar-brand " style = {{color: props.mode==='dark'?'white':'black'}} to="/">{props.title}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" style = {{color: props.mode==='dark'?'white':'black'}} href="/">Home</a>
+            <Link className="nav-link active" aria-current="page" style = {{color: props.mode==='dark'?'white':'black'}} to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" style = {{color: props.mode==='dark'?'white':'black'}} href="/">{props.aboutText}</a>
+          
+          <Link to='About/' className="nav-link" style = {{color: props.mode==='dark'?'white':'black'}} >{props.aboutText}</Link>
          </li>
         </ul>
           <div className="form-check form-switch my-3 mx-5">
@@ -38,3 +41,4 @@ Navbar.defaultProps = {
   title: "Set Title",
   aboutText: "About",
 };
+
